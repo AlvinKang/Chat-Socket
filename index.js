79cc052448fe70ab4f745c9a95a21e02cf2a61e0
Alvin Kang <oneinallin1@gmail.com>
2018-07-12 14:56:49 -0700

    Set up socket.io on back- and front-end

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const socket = require("socket.io");
 
 // App setup
 const app = express();
@@ -9,4 +10,11 @@ app.use(express.static("public"));
 
 const server = app.listen(port, () => {
   console.log(`Listening on port ${port}`);
+});
+
+// Socket setup
+const io = socket(server);
+
+io.on("connection", socket => {
+  console.log("Made socket connection", socket.id);
 });
