7e2caae6b140f3e3afb6d330bad71da21b1b7f31
Alvin Kang <oneinallin1@gmail.com>
2018-07-12 15:28:39 -0700

    Handle when a user is typing

--- index.js
+++ index.js
@@ -18,8 +18,13 @@ const io = socket(server);
 io.on("connection", socket => {
   console.log("Made socket connection", socket.id);
 
-  // When server receives a chat signal, emit the data to ALL clients connected to the server
+  // When server receives a "chat" signal, emit the data to ALL clients connected to the server
   socket.on("chat", data => {
     io.sockets.emit("chat", data);
+  });
+
+  // When server receives a "typing" signal, emit user's handle to ALL clients except the user that's typing
+  socket.on("typing", data => {
+    socket.broadcast.emit("typing", data);
   });
 });
--- public/chat.js
+++ public/chat.js
@@ -6,22 +6,39 @@ const message = document.querySelector("#message");
 const handle = document.querySelector("#handle");
 const btn = document.querySelector("#send");
 const output = document.querySelector("#output");
+const feedback = document.querySelector("#feedback");
 
-// Emit events
+// Listen for user events
 
+// When user clicks send
 btn.addEventListener("click", () => {
-  // Emit this "chat" signal to server with the object
+  // Emit this "chat" signal to server along with the handle and message
   socket.emit("chat", {
     message: message.value,
     handle: handle.value
   });
 });
 
-// Listen for events
+// When a user is typing
+message.addEventListener("keypress", () => {
+  // Emit "typing" signal to server along with user's handle
+  socket.emit("typing", handle.value);
+});
 
+// Listen for events from server
+
+// When server emits a signal with "chat", render the message on the DOM
 socket.on("chat", data => {
+  // Clear any feedback
+  feedback.innerHTML = "";
+
   output.innerHTML += `<p>
   <strong>${data.handle}: </strong>
   ${data.message}
   </p>`;
+});
+
+// When server emits a signal with "typing", render feedback on DOM
+socket.on("typing", data => {
+  feedback.innerHTML = `<p><em>${data} is typing a message...</em></p>`;
 });
