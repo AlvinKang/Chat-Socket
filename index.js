0387b019bf844edb9d78e7c42a698b137f8ef36c
Alvin Kang <oneinallin1@gmail.com>
2018-07-12 15:11:55 -0700

    Add logic to emit messages to all connections when a message is sent

--- index.js
+++ index.js
@@ -17,4 +17,9 @@ const io = socket(server);
 
 io.on("connection", socket => {
   console.log("Made socket connection", socket.id);
+
+  // When server receives a chat signal, emit the data to ALL clients connected to the server
+  socket.on("chat", data => {
+    io.sockets.emit("chat", data);
+  });
 });
--- public/chat.js
+++ public/chat.js
@@ -1,2 +1,27 @@
 // Make connection
 const socket = io.connect("http://localhost:4000");
+
+// Query DOM
+const message = document.querySelector("#message");
+const handle = document.querySelector("#handle");
+const btn = document.querySelector("#send");
+const output = document.querySelector("#output");
+
+// Emit events
+
+btn.addEventListener("click", () => {
+  // Emit this "chat" signal to server with the object
+  socket.emit("chat", {
+    message: message.value,
+    handle: handle.value
+  });
+});
+
+// Listen for events
+
+socket.on("chat", data => {
+  output.innerHTML += `<p>
+  <strong>${data.handle}: </strong>
+  ${data.message}
+  </p>`;
+});
